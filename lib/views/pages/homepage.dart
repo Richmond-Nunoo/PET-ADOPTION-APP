5605cc4fee3c6129a2a175b3ac1d278a4e3e6097
Richmond Nunoo <1957richmond@gmail.com>
2023-01-11 15:08:22 -1200

    code clean up

--- lib/views/pages/homepage.dart
+++ lib/views/pages/homepage.dart
@@ -16,13 +16,14 @@ class _UserHomePageState extends State<UserHomePage> {
   ];
 
   List images = [
-    "assets/images/dog5.jpg",
-    "assets/images/cat.jpg",
-    "assets/images/squirel.jpg"
+    "assets/images/pup.jpg",
+    "assets/images/cat1.jpg",
+    "assets/images/rabbit.jpg"
   ];
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context).textTheme;
     return Scaffold(
       backgroundColor: Colors.grey.shade100,
       appBar: AppBar(
@@ -32,19 +33,20 @@ class _UserHomePageState extends State<UserHomePage> {
           color: Colors.black,
         ),
         title: RichText(
-          text: const TextSpan(
-            style: TextStyle(color: Colors.black, wordSpacing: 5),
+          text: TextSpan(
+            style: theme.bodySmall,
             children: [
-              WidgetSpan(
+              const WidgetSpan(
                   child: Icon(
                 Icons.location_on_sharp,
                 color: Colors.red,
-                size: 15,
+                size: 14,
               )),
               TextSpan(
                   text: " Accra, ",
-                  style: TextStyle(fontWeight: FontWeight.bold)),
-              TextSpan(text: "Ghana"),
+                  style:
+                      theme.bodySmall!.copyWith(fontWeight: FontWeight.bold)),
+              const TextSpan(text: "Ghana"),
             ],
           ),
         ),
@@ -64,189 +66,118 @@ class _UserHomePageState extends State<UserHomePage> {
       ),
       body: ListView(
         physics: const BouncingScrollPhysics(),
-        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "Find a new friend",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          Text(
+            "Find a new friend",
+            style: theme.bodyLarge,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 50,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(
+                Radius.circular(15),
               ),
-              const SizedBox(
-                height: 10,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  const Icon(
+                    Icons.search,
+                    color: Colors.grey,
+                  ),
+                  Text(
+                    " Search for pets",
+                    style: theme.bodySmall,
+                  ),
+                  const Spacer(),
+                  const Icon(
+                    Icons.display_settings_sharp,
+                    color: Colors.grey,
+                  )
+                ],
               ),
-              Container(
-                height: 45,
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(15),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Text(
+            "Pet Categories",
+            style: theme.bodyLarge,
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            physics: const BouncingScrollPhysics(),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: List.generate(
+                categories.length,
+                (index) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        sectedCategory = index;
+                      });
+                    },
+                    child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 500),
+                        height: 55,
+                        width: 110,
+                        decoration: BoxDecoration(
+                          color: sectedCategory == index
+                              ? Colors.red
+                              : Colors.white,
+                          borderRadius: sectedCategory == index
+                              ? BorderRadius.circular(25)
+                              : BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              CircleAvatar(
+                                backgroundImage: AssetImage(images[index]),
+                              ),
+                              Text(
+                                categories[index],
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    overflow: TextOverflow.ellipsis,
+                                    fontWeight: FontWeight.w500,
+                                    color: sectedCategory == index
+                                        ? Colors.white
+                                        : Colors.black),
+                              ),
+                            ],
+                          ),
+                        )
+                        // Center(
+                        //   child: Text(
+                        //     categories[index],
+                        //     style: TextStyle(
+                        //         fontWeight: FontWeight.w500,
+                        //         color: sectedCategory == index
+                        //             ? Colors.white
+                        //             : Colors.black),
+                        //   ),
+                        // ),
+                        ),
                   ),
                 ),
               ),
-              const SizedBox(
-                height: 30,
-              ),
-              const Text(
-                "Pet Categories",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: ListView.builder(
-                    shrinkWrap: true,
-                    physics: const BouncingScrollPhysics(),
-                    scrollDirection: Axis.horizontal,
-                    itemCount: categories.length,
-                    itemBuilder: (context, index) {
-                      return GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            sectedCategory = index;
-                          });
-                         
-                        },
-                        child: AnimatedContainer(
-                          margin: const EdgeInsets.all(7),
-                          duration: const Duration(
-                            milliseconds: 300,
-                          ),
-                          height: 40,
-                          width: 85,
-                          decoration: BoxDecoration(
-                            color: sectedCategory == index
-                                ? Colors.red
-                                : Colors.white,
-                            borderRadius: sectedCategory == index
-                                ? BorderRadius.circular(15)
-                                : BorderRadius.circular(10),
-                          ),
-                          child: Center(
-                            child: Text(
-                              categories[index],
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  color: sectedCategory == index
-                                      ? Colors.white
-                                      : Colors.black),
-                            ),
-                          ),
-                        ),
-                      );
-                    },),
-              ),
-              //      Text(images[sectedCategory])
-
-              // Container(
-              //   height: 50,
-              //   width: 60,
-              //   decoration: const BoxDecoration(
-              //     color: Colors.white,
-              //     borderRadius: BorderRadius.all(
-              //       Radius.circular(10),
-              //     ),
-              //   ),
-              // )
-            ],
-          ),
+            ),
+          )
         ],
-
-        // Container(
-        //     width: 100,
-        //     height: double.infinity,
-        //     margin: const EdgeInsets.all(10),
-        //     child: Column(
-        //       children: [
-        //         SizedBox(
-        //           width: double.infinity,
-        //           height: 60,
-        //           child: ListView.builder(
-        //               shrinkWrap: true,
-        //               physics: const BouncingScrollPhysics(),
-        //               scrollDirection: Axis.horizontal,
-        //               itemCount: categories.length,
-        //               itemBuilder: (context, index) {
-        //                 return Column(
-        //                   children: [
-        //                     GestureDetector(
-        //                       onTap: () {
-        //                         setState(() {
-        //                           sectedCategory = index;
-        //                         });
-        //                       },
-        //                       child: AnimatedContainer(
-        //                         margin: const EdgeInsets.all(7),
-        //                         duration: const Duration(
-        //                           milliseconds: 300,
-        //                         ),
-        //                         height: 80,
-        //                         width: 45,
-        //                         decoration: BoxDecoration(
-        //                           color: sectedCategory == index
-        //                               ? Colors.red
-        //                               : Colors.blue,
-        //                           borderRadius: sectedCategory == index
-        //                               ? BorderRadius.circular(15)
-        //                               : BorderRadius.circular(10),
-        //                           border: sectedCategory == index
-        //                               ? Border.all(
-        //                                   color: Colors.deepPurpleAccent,
-        //                                   width: 2)
-        //                               : null,
-        //                         ),
-        //                         child: Center(
-        //                           child: Text(
-        //                             categories[index],
-        //                             style: TextStyle(
-        //                                 fontWeight: FontWeight.w500,
-        //                                 color: sectedCategory == index
-        //                                     ? Colors.black
-        //                                     : Colors.grey),
-        //                           ),
-        //                         ),
-        //                       ),
-        //                     ),
-        //                     Visibility(
-        //                       visible: sectedCategory == index,
-        //                       child: Container(
-        //                         width: 5,
-        //                         height: 5,
-        //                         decoration: const BoxDecoration(
-        //                             color: Colors.deepPurpleAccent,
-        //                             shape: BoxShape.circle),
-        //                       ),
-        //                     )
-        //                   ],
-        //                 );
-        //               }),
-        //         ),
-        //         Container(
-        //           margin: const EdgeInsets.only(top: 30),
-        //           width: double.infinity,
-        //           height: 550,
-        //           child: Column(
-        //             mainAxisAlignment: MainAxisAlignment.center,
-        //             children: [
-        //               Text(
-        //                 categories[sectedCategory],
-        //               ),
-        //               const SizedBox(
-        //                 height: 10,
-        //               ),
-        //               Text(images[sectedCategory],
-        //                   style: const TextStyle(
-        //                       fontWeight: FontWeight.w500,
-        //                       fontSize: 30,
-        //                       color: Colors.deepPurple)),
-        //             ],
-        //           ),
-        //         ),
-        //       ],
-        //     ))
       ),
     );
   }
